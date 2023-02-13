9f2da67362360d6c3dde91811c128fb7a5b9073e
tms <tomas.rls@gmail.com>
2023-02-13 15:56:49 +0200

    Added task list and detail view

--- tools_tasks/tools_and_tasks/urls.py
+++ tools_tasks/tools_and_tasks/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('', views.start, name='start'),
     path('tools/', views.ToolListView.as_view(), name='tools_list_view'),
     path('tools/<int:pk>', views.ToolDetailView.as_view(), name='tool_detail_view'),
+    path('tasks/', views.TaskListView.as_view(), name='tasks_list_view'),
+    path('tasks/<int:pk>', views.TaskDetailView.as_view(), name='task_detail_view'),
 ]
--- tools_tasks/tools_and_tasks/views.py
+++ tools_tasks/tools_and_tasks/views.py
@@ -1,7 +1,7 @@
 
 from django.shortcuts import render
 from django.views import generic
-from .models import Tool
+from .models import Tool, Task
 
 
 def start(request):
@@ -18,3 +18,15 @@ class ToolDetailView(generic.DetailView):
     model = Tool
     template_name = 'tool_detail.html'
     context_object_name = "tool_detail"
+
+
+class TaskListView(generic.ListView):
+    model = Task
+    template_name = 'tasks_list.html'
+    context_object_name = "tasks_list"
+
+
+class TaskDetailView(generic.DetailView):
+    model = Task
+    template_name = 'task_detail.html'
+    context_object_name = "task_detail"
